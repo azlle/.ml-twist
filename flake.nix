e326ea5a4ba87e0c59353dcc3aeecb21fd96755e
Azlle <moxmo2@pm.me>
2026-07-29 20:31:46 +0900

    Add flake-parts to support building on aarch64-linux

--- flake.nix
+++ flake.nix
@@ -8,7 +8,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+    twist.url = "git+https://upd.dev/Azlle/twist.nix";
+    org-babel.url = "git+https://upd.dev/Azlle/org-babel";
 
     elpa = {
       url = "github:elpa-mirrors/elpa";
@@ -29,96 +35,91 @@
       url = "github:emacsmirror/epkgs";
       flake = false;
     };
-
-    twist.url = "git+https://upd.dev/Azlle/twist.nix";
-    org-babel.url = "git+https://upd.dev/Azlle/org-babel";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      emacs-overlay,
-      ...
-    }@inputs:
+    inputs@{ self, nixpkgs, flake-parts, emacs-overlay, ... }:
 
-    let
-      inherit (nixpkgs) lib;
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          emacs-overlay.overlays.default
-          inputs.org-babel.overlays.default
-        ];
-      };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
 
-      profile = {
-        lockDir = ./lock;
-        earlyInitFile = (pkgs.tangleOrgBabelFile "early-init.el" ./early-init.org {});
-        initFiles = [ (pkgs.tangleOrgBabelFile "init.el" ./emacs-config.org {}) ];
-        exportManifest = true;
-        emacsPackage = pkgs.emacs-git-pgtk;
-      };
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              emacs-overlay.overlays.default
+              inputs.org-babel.overlays.default
+            ];
+          };
 
-      # lib.makeEnvはEmacsの環境構築用の関数らしい
-      package = inputs.twist.lib.makeEnv {
-        inherit pkgs;
+          profile = {
+            lockDir = ./lock;
+            earlyInitFile = pkgs.tangleOrgBabelFile "early-init.el" ./early-init.org { };
+            initFiles = [ (pkgs.tangleOrgBabelFile "init.el" ./emacs-config.org { }) ];
+            exportManifest = true;
+            emacsPackage = pkgs.emacs-git-pgtk;
+          };
 
-        # (emacsProfile)と置いておくとemacsProfile.lockDirではなくlockDirとしてinheritできるらしい
-        # lib.makeEnvではearlyInitFileを使わないのでinheritしてはいけないですよ(一敗)
-        inherit (profile) emacsPackage lockDir initFiles exportManifest;
+          package = inputs.twist.lib.makeEnv {
+            inherit pkgs;
+            inherit (profile) emacsPackage lockDir initFiles exportManifest;
 
-        registries = import ./nix/registries.nix {
-          inherit inputs;
-          inherit (profile) emacsPackage;
-        } ++ [ ];
-      };
+            registries = import ./nix/registries.nix {
+              inherit inputs;
+              inherit (profile) emacsPackage;
+            } ++ [ ];
+          };
 
-      defaultWrapper = pkgs.callPackage ./nix/tmpInitDirWrapper.nix { } {
-        emacsEnv = package;
-        inherit (profile) initFiles earlyInitFile;
-        assetsDir = ./assets;
-        # snippetsDir = ./snippets;
-        manifestFile = package.emacsWrapper.elispManifestPath;
-      };
+          defaultWrapper = pkgs.callPackage ./nix/tmpInitDirWrapper.nix { } {
+            emacsEnv = package;
+            inherit (profile) initFiles earlyInitFile;
+            assetsDir = ./assets;
+            manifestFile = package.emacsWrapper.elispManifestPath;
+          };
+        in
+        {
+          packages.default = package;
+          # home-manager モジュール側から拾えるように earlyInitFile も出力しておく
+          packages.earlyInitFile = profile.earlyInitFile;
 
-    in {
-      packages.${system}.default = package;
-      apps.${system} = package.makeApps { lockDirName = "lock"; }
-      // {
-        default = {
-          type = "app";
-          program = "${defaultWrapper}/bin/emacs-twist";
-        };
-      };
-
-      homeModules.twist = { lib, ... }: {
-        imports = [
-          inputs.twist.homeModules.emacs-twist
-        ];
-
-        programs.emacs-twist = {
-          config = lib.mkDefault package;
-          earlyInitFile = lib.mkDefault profile.earlyInitFile;
-          createManifestFile = lib.mkDefault true;
+          apps = package.makeApps { lockDirName = "lock"; } // {
+            default = {
+              type = "app";
+              program = "${defaultWrapper}/bin/emacs-twist";
+            };
+          };
         };
 
-        home.file = {
-          ".config/emacs/assets/".source = ./assets;
-        };
-
-        home.packages =
-          with pkgs;
-          [
-            adwaita-icon-theme
-            adwaita-icon-theme-legacy
-            ffmpeg
-            gcc
-            skkDictionaries.l
-            vips
-            wl-clipboard
+      flake = {
+        homeModules.twist = { lib, pkgs, ... }: {
+          imports = [
+            inputs.twist.homeModules.emacs-twist
           ];
+
+          programs.emacs-twist = {
+            config = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+            earlyInitFile = lib.mkDefault self.packages.${pkgs.stdenv.hostPlatform.system}.earlyInitFile;
+            createManifestFile = lib.mkDefault true;
+          };
+
+          home.file = {
+            ".config/emacs/assets/".source = ./assets;
+          };
+
+          home.packages =
+            with pkgs;
+            [
+              adwaita-icon-theme
+              adwaita-icon-theme-legacy
+              ffmpeg
+              gcc
+              skkDictionaries.l
+              vips
+              wl-clipboard
+            ];
+        };
       };
     };
 }
